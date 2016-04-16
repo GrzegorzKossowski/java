75bd0baf21bd8c697d5ebcd60972adde717ad107
Grzegorz Kossowski <grzegorz.kossowski@gmail.com>
2016-04-16 17:29:13 +0200

    Algorithms

--- Algorithms/src/algorithm/SumOfNumber.java
+++ Algorithms/src/algorithm/SumOfNumber.java
@@ -2,19 +2,20 @@ package algorithm;
 
 /**
  * Find sum of all digits in given number (int).
- * @author grze
- *
+ * @author grze<br>
+ * <a href="http://www.codefights.com">source of challenge</a> *
  */
 public class SumOfNumber {
 	
 	public static int countSumOfNumber (int num) {
-		int sum = 0; 
+		
+		int sum = 0; //sum of integer digits 
 		while (num > 9) {
 			int modulo = num%10;
 			sum +=  modulo;
 			num /= 10;		
 		}
-		sum += num;
+		sum += num;	//if num <10
 		return sum;		
 	}
 	
